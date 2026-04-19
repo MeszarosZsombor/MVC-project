717187da68ff3771bc948970c473c8ded6ff391f
zsombor <mzsombor10@gmail.com>
2026-04-19 22:59:33 +0200

    if user is logged in a new component appears - Profile (not finished)

---
+++ frontend/src/components/Profile.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import { ref } from 'vue';
+  import { useAuthStore } from "../stores/auth.js";
+
+  const auth = useAuthStore();
+  const user = auth.user;
+  const adoptions = ref([]);
+
+  console.log(user);
+</script>
+
+<template>
+  Welcome {{ user.name }}!
+
+  Your adoptions:
+  <div class="adoptions">
+    <div v-for="adoption in adoptions" class="pet-card">
+      <p>Pet name: </p>
+      <p> Adoption date: </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/src/views/Users.vue
+++ frontend/src/views/Users.vue
@@ -3,13 +3,20 @@
 import { ref } from "vue";
 import Register from "../components/Register.vue";
 import Login from "../components/Login.vue";
+import { useAuthStore } from "../stores/auth.js";
+import Profile from "../components/Profile.vue";
 
 const activeTab = ref("login");
+const auth = useAuthStore();
 </script>
 
 <template>
-  <Login v-if="activeTab === 'login'" @switch="activeTab = 'register'" />
-  <Register v-else @switch="activeTab = 'login'" />
+  <Profile v-if="auth.isLoggedIn"/>
+
+  <div v-else>
+    <Login v-if="activeTab === 'login'" @switch="activeTab = 'register'" />
+    <Register v-else @switch="activeTab = 'login'" />
+  </div>
 </template>
 
 <style scoped>
