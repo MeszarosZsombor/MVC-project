54272ef470ce15a32b85ee0baa1c482784e32616
zsombor <mzsombor10@gmail.com>
2026-04-21 21:44:46 +0200

    error handling changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -6,17 +6,13 @@ const api = axios.create({
 })
 
 api.interceptors.request.use(config => {
-    const authStore = useAuthStore()
-    
-    console.log("TOKEN:", localStorage.getItem("token"))
-    console.log("INTERCEPTOR HIT");
-
-    if (authStore.token) {
-        config.headers.Authorization = `Bearer ${authStore.token}`
+    const token = localStorage.getItem('token');
+    console.log("TOKEN:", token);
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
     }
-
-    return config
-})
+    return config;
+});
 
 api.interceptors.response.use(
     response => response,
@@ -25,8 +21,11 @@ api.interceptors.response.use(
 
         const isAuthRoute = error.config.url.includes("/auth");
 
-        if (!isAuthRoute && error.response?.status === 401 || error.response?.status === 403) {
+        if (!isAuthRoute && (error.response?.status === 401)) {
             console.log("Unauthorized access... logging out user...");
+            authStore.logout();
+        } else if (!isAuthRoute && (error.response?.status === 403)){
+            console.log("Something went wrong...");
             authStore.logout();
         }
 
